69e80bb2c7e303358d8d64548da44804e81e0828
unknown <kawsarahmed4148@gmail.com>
2022-07-25 08:26:13 +0600

    Country details added

--- js/countries.js
+++ js/countries.js
@@ -15,13 +15,34 @@ const displayCountries = countries => {
 const countriesDiv = document.getElementById('countries')
 
 countries.forEach(country => {
+   
     const div = document.createElement('div')
-    const h3 = document.createElement('h3');
-    h3.innerText = country.name;
-    div.appendChild(h3)
-    const p = document.createElement('p')
-    p.innerText = country.capital;
-    div.appendChild(p)
+    div.classList.add('country')
+    div.innerHTML = `
+    <h3>${country.name}</h3>
+    <p>${country.capital}</p>
+    <button onclick = "loadCountryByName('${country.name}')">Details</button>
+    `
+
     countriesDiv.appendChild(div)
 });
+
+}
+
+const loadCountryByName  = name =>{
+    fetch(`https://restcountries.com/v2/name/${name}`)
+    .then(res => res.json())
+    .then(data => displayCountriesDetails(data[0]))
+}
+
+
+const displayCountriesDetails = country => {
+    console.log(country);
+    const countryDetails = document.getElementById('country-details')
+    countryDetails.innerHTML = `
+    <h5>${country.name}</h5>
+    <p>Population: ${country.population}</p>
+
+    <img width="150px" src ="${country.flag}">
+    `
 }
